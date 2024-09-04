1018daab2a79d2afc7b0bd2639eedd44c510c4cf
frnki <efembock@c2r1p2.42vienna.com>
2024-09-04 18:51:43 +0200

    added ft_memset.c

---
+++ libft/ft_memset.c
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memset.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: efembock <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/04 18:13:30 by efembock          #+#    #+#             */
+/*   Updated: 2024/09/04 18:49:42 by efembock         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+
+void	*ft_memset(void *s, int c, size_t n)
+{
+	unsigned char	*ptr;
+	unsigned char	value;
+	size_t			i;
+
+	if (s == NULL || n == 0)
+		return (s);
+	ptr = s;
+	value = (unsigned char)c;
+	i = 0;
+	while (i < n)
+	{
+		ptr[i] = value;
+		i++;
+	}
+	return (s);
+}
+/*
+int	main(void)
+{
+	char	str[11] = "1234567890";
+
+	ft_memset(str, 88, 5);
+	printf("%s\n", str);
+	return (0);
+}*/
