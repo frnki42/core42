66d32caefb34e1371e5d12f825ad90276a72de5e
frnki <efembock@c2r1p2.42vienna.com>
2024-09-04 18:01:45 +0200

    added ft_strnstr.c

---
+++ libft/ft_strnstr.c
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strnstr.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: efembock <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/04 16:53:20 by efembock          #+#    #+#             */
+/*   Updated: 2024/09/04 18:00:18 by efembock         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+
+char	*ft_strnstr(const char *big, const char *little, size_t len)
+{
+	size_t	i;
+	size_t	j;
+
+	i = 0;
+	if (*little == '\0')
+		return ((char *)big);
+	while (big[i] && i < len)
+	{
+		j = 0;
+		while (little[j] && (i + j) < len && big[i + j] == little[j])
+			j++;
+		if (little[j] == '\0')
+			return ((char *)&big[i]);
+		i++;
+	}
+	return (NULL);
+}
+/*
+int	main(void)
+{
+	const char	big[] = "I am big but I got little inside me";
+	const char	little[] = "little";
+
+	printf("%s\n", ft_strnstr(big, little, 42));
+	return (0);
+}*/
