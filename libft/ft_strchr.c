3b56b350a1f516c89bc395c7d918075a89706332
frnki <efembock@c2r1p2.42vienna.com>
2024-09-04 16:09:39 +0200

    added ft_strchr.c

---
+++ libft/ft_strchr.c
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strchr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: efembock <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/04 12:02:30 by efembock          #+#    #+#             */
+/*   Updated: 2024/09/04 16:08:36 by efembock         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+
+char	*ft_strchr(const char *s, int c)
+{
+	int			i;
+
+	i = 0;
+	while (s[i])
+	{
+		if (s[i] == c)
+		{
+			return ((char *)&s[i]);
+		}
+		i++;
+	}
+	if (c == '\0')
+		return ((char *)&s[i]);
+	return (NULL);
+}
+/*
+int	main(void)
+{
+	int		ch;
+
+	ch = 100;
+	printf("%s\n", ft_strchr("Supdawg?", ch));
+	return (0);
+}*/
