074223995c59e5283249ad0e99ad3125084459f7
.frnki <frnki@frnki.com>
2024-09-18 12:45:07 +0200

    modified: Makefile and ft_lstmap.c. Added ft_lstclear.c

--- libft/ft_lstmap.c
+++ libft/ft_lstmap.c
@@ -10,9 +10,42 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdlib.h>
 #include "libft.h"
 
 t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
+	t_list	*new_list;
+	t_list	*new_node;
 	
+	if (!lst || !f || !del)
+		return (NULL);
+	new_list = NULL;
+	while (lst)
+	{
+		new_node = ft_lstnew(f(lst->content));
+		if (!new_node)
+		{
+			ft_lstclear(&new_list, del);
+			return (NULL);
+		}
+		ft_lstadd_back(&new_list, new_node);
+		lst = lst->next;
+	}
+	return (new_list);
 }
+/*
+#include <stdio.h>
+
+int	main(void)
+{
+	t_list	*lst;
+	t_list	*str;
+	char *a;
+
+	a = "i am content!";
+	lst = ft_lstnew(a);
+	str = ft_lstmap(lst, upper, ft_lstdelone);
+	printf("%s\n",(char *)str);
+	return (0);
+}*/
