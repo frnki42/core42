8e2d4b59f9faab1cdbd70b7f09c49f5137db9989
frnki <efembock@c2r1p2.42vienna.com>
2024-11-12 13:20:29 +0100

    improved inter.c

--- grademe/inter/inter.c
+++ grademe/inter/inter.c
@@ -1,56 +1,23 @@
 #include <unistd.h>
 
-int	ft_strlen(char *string)
+void	inter(char *hay, char *needle)
 {
+	int	check[255] = {0};
 	int	i;
-
-	if (!string)
-		return (0);
-	while (string[i])
-		i++;
-	return (i);
-}
-
-int	check_second(char first, char *second)
-{
-	int	i;
+	int	j;
 
 	i = -1;
-	while (second[++i])
+	while (hay[++i])
 	{
-		if (second[i] == first)
-			return (1);
-	}
-	return (0);
-}
-
-int	check_existance(char *first, int index)
-{
-	int	i;
-
-	i = 0;
-	while (i < index)		//traverse first string
-	{				//check if char exists in string already
-		if (first[index] == first[i])
-			return (1);
-		i++;
-	}
-	return (0);
-}
-
-void	inter(char *first, char *second)
-{
-	int	i;
-
-	i = 0;
-	while (first[i])		//traverse first string
-	{				//check if char exists in string already
-		if (check_existance(first, i) == 0)
+		j = -1;
+		while (needle[++j])
 		{
-			if (check_second(first[i], second) == 1)
-				write(1, &first[i], 1);
+			if (hay[i] == needle[j] && check[(int)needle[j]] == 0)
+			{
+				write(1, &hay[i], 1);
+				check[(int)needle[j]] = 1;
+			}
 		}
-		i++;
 	}
 }
 
@@ -62,13 +29,3 @@ int	main(int argc, char **argv)
 	write(1, "\n", 1);
 	return (0);
 }
-/*
-
-int	main(int argc, char **argv)
-{
-	if (argc != 3)
-		return (write(1, "\n", 1);
-	inter(argv[1], argv[2]);
-	write(1, "\n", 1);
-	return (0);
-}*/
