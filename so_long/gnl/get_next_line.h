026432d86eca893b49e3de6d1f85df56e9021694
frnki42 <femboeckelia@googlemail.com>
2024-11-21 16:43:47 +0100

    modified so_long/main.c and added gnl plus some new textures

---
+++ so_long/gnl/get_next_line.h
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: efembock <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/11 11:14:59 by efembock          #+#    #+#             */
+/*   Updated: 2024/10/26 15:28:34 by efembock         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <stdlib.h>
+# include <unistd.h>
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 1024
+# endif
+
+size_t	ft_strlen(char *s);
+char	*ft_strchr(char *s, int c);
+char	*ft_strjoin(char *s1, char *s2);
+
+char	*read_file(int fd, char *rest);
+char	*ft_get_line(char *rest);
+char	*update_rest(char *rest);
+char	*get_next_line(int fd);
+
+#endif
--- so_long/main.c
+++ so_long/main.c
@@ -8,10 +8,10 @@
 
 typedef struct	s_vars
 {
+	char	**map;
 	int	img_width;
 	int	img_height;
-	void	*img;
-	void	*img2;
+	void	*img[5];
 	void	*mlx;
 	void	*win;
 }	t_vars;
@@ -39,7 +39,7 @@ int	print_floor(t_vars *vars)
 		x = 0;
 		while (x <= WIDTH)
 		{
-			mlx_put_image_to_window(vars->mlx, vars->win,vars->img, x, y);
+			mlx_put_image_to_window(vars->mlx, vars->win,vars->img[0], x, y);
 			x += 32;	
 		}
 		y += 32;
@@ -52,26 +52,33 @@ int	print_checker(t_vars *vars)
 	int	x;
 	int	y;
 
-	y = 16;
+	y = 0;
 	while (y <= HEIGHT)
 	{	
-		x = 16;
+		x = 0;
 		while (x <= WIDTH)
 		{
-			mlx_put_image_to_window(vars->mlx, vars->win,vars->img2, x, y);
+			mlx_put_image_to_window(vars->mlx, vars->win,vars->img[1], x, y);
+			mlx_put_image_to_window(vars->mlx, vars->win,vars->img[2], x + 32, y);
 			x += 64;	
 		}
-		y += 64;
+		y += 32;
 	}
 	return (0);
 }
 
 int	exit_game(t_vars *vars)
 {
-	if (vars->img)
-		mlx_destroy_image(vars->mlx, vars->img);
-	if (vars->img2)
-		mlx_destroy_image(vars->mlx, vars->img2);
+	if (vars->img[0])
+		mlx_destroy_image(vars->mlx, vars->img[0]);
+	if (vars->img[1])
+		mlx_destroy_image(vars->mlx, vars->img[1]);
+	if (vars->img[2])
+		mlx_destroy_image(vars->mlx, vars->img[2]);
+	if (vars->img[3])
+		mlx_destroy_image(vars->mlx, vars->img[3]);
+	if (vars->img[4])
+		mlx_destroy_image(vars->mlx, vars->img[4]);
 	if (vars->win)
 		mlx_destroy_window(vars->mlx, vars->win);
 	exit (0);
@@ -105,21 +112,48 @@ int	key_hook(int keycode, t_vars *vars)
 int	mouse_click(int mouse_button, int x, int y, t_vars *vars)
 {
 	if (mouse_button == 1)
-		mlx_string_put(vars->mlx, vars->win, x, y, AQUA_DARK, "IIIIIIIIII");
+		mlx_string_put(vars->mlx, vars->win, x, y, AQUA_DARK, "4242424242");
 	if (mouse_button == 2)
-		mlx_string_put(vars->mlx, vars->win, x, y, FG_1 , "IIIIIIIIII");
+		mlx_string_put(vars->mlx, vars->win, x, y, FG_1 , "4242424242");
 	if (mouse_button == 3)
-		mlx_string_put(vars->mlx, vars->win, x, y, AQUA_LIGHT, "IIIIIIIIII");
+		mlx_string_put(vars->mlx, vars->win, x, y, AQUA_LIGHT, "4242424242");
 	if (mouse_button == 4)
-		mlx_string_put(vars->mlx, vars->win, x, y, 0x98971a, "IIIIIIIIII");
+		mlx_string_put(vars->mlx, vars->win, x, y, 0xd3869b, "4242424242");
 	if (mouse_button == 5)
-		mlx_string_put(vars->mlx, vars->win, x, y, 0xfb4934, "IIIIIIIIII");
+		mlx_string_put(vars->mlx, vars->win, x, y, 0x83a598, "4242424242");
 	if (mouse_button == 8)
+		print_checker(vars);
+	if (mouse_button == 9)
 		print_checker(vars);
 	printf("mouse button [%d] clicked at (%d, %d)\n", mouse_button, x, y);
 	return (0);
 }
+/*
+int	display_map(t_vars vars)
+{
+	int	y;
+	int	x;
 
+	y = 0;
+	while (vars->map[y])
+	{
+		x = 0;
+		while (vars->map[y][x])
+		{
+			if (vars->map[y][x] == '0')
+				mlx_put_image_to_window(vars->mlx, vars->win,vars->img[0], x, y);
+			if (vars->map[y][x] == '1')
+				mlx_put_image_to_window(vars->mlx, vars->win,vars->img[1], x, y);
+			if (vars->map[y][x] == 'C')
+				mlx_put_image_to_window(vars->mlx, vars->win,vars->img[2], x, y);
+			if (vars->map[y][x] == 'E')
+				mlx_put_image_to_window(vars->mlx, vars->win,vars->img[3], x, y);
+			if (vars->map[y][x] == 'P')
+				mlx_put_image_to_window(vars->mlx, vars->win,vars->img[4], x, y);
+		}
+	}
+}
+*/
 int	main(void)
 {
 	t_vars	vars;
@@ -131,17 +165,14 @@ int	main(void)
 	if (!vars.mlx)
 		return (1);
 	vars.win = mlx_new_window(vars.mlx, WIDTH, HEIGHT, ".frnki");
-	if (!vars.win)
+	vars.img[0] = mlx_xpm_file_to_image(vars.mlx, "textures/grey_background.xpm", &vars.img_width, &vars.img_height);
+	vars.img[1] = mlx_xpm_file_to_image(vars.mlx, "textures/green_light.xpm", &vars.img_width, &vars.img_height);
+	vars.img[2] = mlx_xpm_file_to_image(vars.mlx, "textures/green_dark.xpm", &vars.img_width, &vars.img_height);
+	vars.img[3] = mlx_xpm_file_to_image(vars.mlx, "textures/aqua_light.xpm", &vars.img_width, &vars.img_height);
+	vars.img[4] = mlx_xpm_file_to_image(vars.mlx, "textures/orange_light.xpm", &vars.img_width, &vars.img_height);
+	if (!vars.img[0] || !vars.img[1] || !vars.img[2] || !vars.img[3] || !vars.img[4])
 		return (free(vars.mlx), 1);
-	vars.img = mlx_xpm_file_to_image(vars.mlx, "textures/grey_background.xpm",
-			&vars.img_width, &vars.img_height);
-	if (!vars.img)
-		return (free(vars.mlx), 1);
-	vars.img2 = mlx_xpm_file_to_image(vars.mlx, "textures/green_dark.xpm",
-			&vars.img_width, &vars.img_height);
-	if (!vars.img2)
-		return (free(vars.mlx), 1);
-	mlx_do_key_autorepeaton(vars.mlx);
+	mlx_do_key_autorepeaton(vars.mlx);			// change to off later!!
 	mlx_hook(vars.win, 2, 1L<<0, key_hook, &vars);
 	mlx_hook(vars.win, 7, 1L<<4, print_hello, &vars);
 	mlx_hook(vars.win, 8, 1L<<5, print_bye, &vars);
