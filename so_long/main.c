222a0717bace775fe25f450c36c2bdc1badd98a5
frnki42 <femboeckelia@googlemail.com>
2024-11-21 11:27:23 +0100

    modified so_long/main.c and restructured the directory

--- so_long/main.c
+++ so_long/main.c
@@ -27,6 +27,25 @@ int	print_hello(t_vars *vars)
 	return (0);
 }
 
+int	print_floor(t_vars *vars)
+{
+	int	x;
+	int	y;
+
+	y = 0;
+	while (y <= HEIGHT)
+	{	
+		x = 0;
+		while (x <= WIDTH)
+		{
+			mlx_put_image_to_window(vars->mlx, vars->win,vars->img, x, y);
+			x += 32;	
+		}
+		y += 32;
+	}
+	return (0);
+}
+
 int	exit_game(t_vars *vars)
 {
 	if (vars->img)
@@ -39,29 +58,40 @@ int	exit_game(t_vars *vars)
 
 int	key_hook(int keycode, t_vars *vars)
 {
-	printf("'%c' was pressed. (keycode: %i)\n", (char)keycode, keycode);
-	if (keycode == 65307)
-		exit_game(vars);
-	if (keycode == 120)
-	{
+	if (keycode == ' ')
+		printf("SPAWN!");
+	if (keycode == 'a')
+		printf("LEFT!");
+	if (keycode == 'd')
+		printf("RIGHT!");
+	if (keycode == 's')
+		printf("DOWN!");
+	if (keycode == 'w')
+		printf("UP!");
+	if (keycode == 'r')
+		printf("RELOAD!");
+	if (keycode == 65288)			// backspace
 		mlx_clear_window(vars->mlx, vars->win);
-		printf("window cleared\n");
-	}
+	if (keycode == 65293)			// enter
+		print_floor(vars);
+	if (keycode == 65307)			// escape
+		exit_game(vars);
+	printf("You pressed: %c (keycode: %i)\n", (char)keycode, keycode);
 	return (0);
 }
 
 int	mouse_click(int mouse_button, int x, int y, t_vars *vars)
 {
 	if (mouse_button == 1)
-		mlx_string_put(vars->mlx, vars->win, x, y, AQUA_DARK, "I <3 MARY!");
+		mlx_string_put(vars->mlx, vars->win, x, y, AQUA_DARK, "IIIIIIIIII");
 	if (mouse_button == 2)
-		mlx_string_put(vars->mlx, vars->win, x, y, FG_1 , "I <3 MARY!");
+		mlx_string_put(vars->mlx, vars->win, x, y, FG_1 , "IIIIIIIIII");
 	if (mouse_button == 3)
-		mlx_string_put(vars->mlx, vars->win, x, y, AQUA_LIGHT, "I <3 MARY!");
+		mlx_string_put(vars->mlx, vars->win, x, y, AQUA_LIGHT, "IIIIIIIIII");
 	if (mouse_button == 4)
-		mlx_string_put(vars->mlx, vars->win, x, y, 0x98971a, "I <3 MARY!");
+		mlx_string_put(vars->mlx, vars->win, x, y, 0x98971a, "IIIIIIIIII");
 	if (mouse_button == 5)
-		mlx_string_put(vars->mlx, vars->win, x, y, 0xfb4934, "I <3 MARY!");
+		mlx_string_put(vars->mlx, vars->win, x, y, 0xfb4934, "IIIIIIIIII");
 	if (mouse_button == 8)
 		mlx_put_image_to_window(vars->mlx, vars->win,vars->img, x, y);
 	printf("mouse button [%d] clicked at (%d, %d)\n", mouse_button, x, y);
@@ -81,7 +111,8 @@ int	main(void)
 	vars.win = mlx_new_window(vars.mlx, WIDTH, HEIGHT, ".frnki");
 	if (!vars.win)
 		return (free(vars.mlx), 1);
-	vars.img = mlx_xpm_file_to_image(vars.mlx, "floor.xpm", &vars.img_width, &vars.img_height);
+	vars.img = mlx_xpm_file_to_image(vars.mlx, "textures/grey_background.xpm",
+			&vars.img_width, &vars.img_height);
 	if (!vars.img)
 		return (free(vars.mlx), 1);
 	mlx_hook(vars.win, 2, 1L<<0, key_hook, &vars);
@@ -91,51 +122,3 @@ int	main(void)
 	mlx_hook(vars.win, 17, 0L, exit_game, &vars);
 	mlx_loop(vars.mlx);
 }
-
-/*
-	while (x < WIDTH)
-	{
-		y = 200;
-		while (y < HEIGHT - 200)
-		{
-			mlx_pixel_put(vars.mlx, vars.win, x, y, AQUA_DARK);
-			y++;
-		}
-		x++;
-	}
-	x = 0;
-	while (x < WIDTH * 0.8)
-	{
-		y = 300;
-		while (y < HEIGHT - 300)
-		{
-			mlx_pixel_put(vars.mlx, vars.win, x, y, BG_0);
-			y++;
-		}
-		x++;
-	}
-	x = 0;
-	while (x < WIDTH * 0.7)
-	{
-		y = 330;
-		while (y < HEIGHT - 330)
-		{
-			mlx_pixel_put(vars.mlx, vars.win, x, y, FG_1);
-			y++;
-		}
-		x++;
-	}
-
-int	close(int keycode, t_vars *vars)
-{
-	mlx_destroy_window(vars->mlx, vars->win);
-	return (0);
-}*/
-/*
-int	mouse_hook(int mousecode, t_vars *vars)
-{
-	printf("mousecode: %i\n", mousecode);
-	return (0);
-}
-	mlx_hook(vars.win, 4, 1l<<2, mouse_hook, &vars);
-*/
