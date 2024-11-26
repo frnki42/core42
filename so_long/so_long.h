1da619e39f07912feba7ca4cf94e7fb09864c172
frnki42 <femboeckelia@googlemail.com>
2024-11-26 04:59:11 +0100

    moved some functions to other files. messed it up somehow. gonna fix later

--- so_long/so_long.h
+++ so_long/so_long.h
@@ -81,12 +81,13 @@ void	init_mary(t_game *game);
 void	display_mary(t_game *game);
 void	convert_ber(t_game *game, char *path);
 void	convert_str(t_game *game);
-int	check_rectangle(t_game *game);
 void	render_map(t_game *game);
 void	manipulate_map(t_game *game);
 int	check_collectible(t_game *game);
 int	check_exit(t_game *game);
 int	check_player(t_game *game);
+int	check_rectangle(t_game *game);
 int	check_wall(t_game *game, int keycode);
+int	check_valid(t_game *game);
 void	count_collectibles(t_game *game);
 #endif
