9dac55788f5e6dfdb0427a865577d44ea64f11d0
frnki42 <femboeckelia@googlemail.com>
2024-11-28 02:56:56 +0100

    sorted prototypes alphabetically

--- so_long/so_long.h
+++ so_long/so_long.h
@@ -23,8 +23,6 @@
 # include "libft/libft.h"
 
 // keys
-# define KEY_BACKSPACE	65288
-# define KEY_ENTER	65293
 # define KEY_ESCAPE	65307
 // typedef & structs
 
@@ -65,41 +63,41 @@ typedef struct	s_game
 }	t_game;
 
 // prototypes
-int	movement(int keycode, t_game *game);
-int	exit_game(t_game *game);
-void	load_game_textures(t_game *game);
-void	load_mary(t_game *game);
-void	start_mlx(t_game *game);
+int	check_argv(char *argv);
+int	check_collectible(t_game *game);
+int	check_composition(t_game *game);
+int	check_exit(t_game *game);
+int	check_path(t_game *game);
+int	check_player(t_game *game);
+int	check_rectangle(t_game *game);
+int	check_surrounded(t_game *game);
+int	check_wall(t_game *game, int keycode);
+void	check_valid(t_game *game);
+void	convert_ber(t_game *game, char *path);
+void	convert_str(t_game *game);
+void	count_collectibles(t_game *game);
 void	create_window(t_game *game);
+void	destroy_stuff(t_game *game);
+void	detect_start(t_game *game);
+void	display_mary(t_game *game);
+void	flood_fill(t_game *game, size_t y, size_t x);
+void	free_stuff(t_game *game);
+void	initialize_game(t_game *game);
 void	init_game(t_game *game);
 void	init_map(t_game *game);
 void	init_mary(t_game *game);
-void	display_mary(t_game *game);
-void	convert_ber(t_game *game, char *path);
-void	convert_str(t_game *game);
-int	check_rectangle(t_game *game);
-void	render_map(t_game *game);
+void	load_game_textures(t_game *game);
+void	load_mary(t_game *game);
 void	manipulate_map(t_game *game);
-int	check_collectible(t_game *game);
-int	check_exit(t_game *game);
-int	check_player(t_game *game);
-int	check_wall(t_game *game, int keycode);
-void	count_collectibles(t_game *game);
-void	check_valid(t_game *game);
-int	check_surrounded(t_game *game);
-int	check_composition(t_game *game);
-void	detect_start(t_game *game);
-void	flood_fill(t_game *game, size_t y, size_t x);
-int	check_path(t_game *game);
-int	check_argv(char *argv);
-void	initialize_game(t_game *game);
-void	destroy_stuff(t_game *game);
-void	free_stuff(t_game *game);
-void	prepare_game(t_game *game, char *path);
-void	start_game(t_game *game);
+int	movement(int keycode, t_game *game);
+void	move_down(int keycode, t_game *game);
 void	move_left(int keycode, t_game *game);
 void	move_right(int keycode, t_game *game);
-void	move_down(int keycode, t_game *game);
 void	move_up(int keycode, t_game *game);
+int	exit_game(t_game *game);
+void	prepare_game(t_game *game, char *path);
+void	render_map(t_game *game);
 void	show_map(t_game *game, int x, int y);
+void	start_game(t_game *game);
+void	start_mlx(t_game *game);
 #endif
