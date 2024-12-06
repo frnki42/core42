27b6760519c9de90b2c96e26b6935797f0019ed0
frnki42 <femboeckelia@googlemail.com>
2024-12-06 01:42:02 +0100

    moved init_structs() to utils.c

--- push_swap/utils.c
+++ push_swap/utils.c
@@ -46,3 +46,11 @@ long	ft_atolong(const char *nptr)
 	return (result * either);
 }
 
+void	init_structs(t_data *data)
+{
+	data->input = NULL;
+	data->size = 0;
+	data->stack_a = NULL;
+	data->stack_b = NULL;
+	data->stack_c = NULL;
+}
