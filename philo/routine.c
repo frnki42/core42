9aacbcfb8a45b7f55937d7db444043b908bc0078
frnki42 <femboeckelia@googlemail.com>
2025-02-25 13:19:41 +0100

    protected all printf calls with mutex

--- philo/routine.c
+++ philo/routine.c
@@ -17,23 +17,33 @@ void	pick_up_forks(t_philo *philo)
 	if (philo->num % 2)
 	{
 		pthread_mutex_lock(philo->fork_right);
+		pthread_mutex_lock(&philo->table->msg_lock);
 		printf("%li %i has taken a fork (right)\n", check_time(), philo->num);
+		pthread_mutex_unlock(&philo->table->msg_lock);
 		pthread_mutex_lock(philo->fork_left);
+		pthread_mutex_lock(&philo->table->msg_lock);
 		printf("%li %i has taken a fork (left)\n", check_time(), philo->num);
+		pthread_mutex_unlock(&philo->table->msg_lock);
 	}
 	else
 	{
 		pthread_mutex_lock(philo->fork_left);
+		pthread_mutex_lock(&philo->table->msg_lock);
 		printf("%li %i has taken a fork (left)\n", check_time(), philo->num);
+		pthread_mutex_unlock(&philo->table->msg_lock);
 		pthread_mutex_lock(philo->fork_right);
+		pthread_mutex_lock(&philo->table->msg_lock);
 		printf("%li %i has taken a fork (right)\n", check_time(), philo->num);
+		pthread_mutex_unlock(&philo->table->msg_lock);
 	}
 }
 
 // "eat" until time is gone
 void	eat_spaghetti(t_philo *philo)
 {
+	pthread_mutex_lock(&philo->table->msg_lock);
 	printf("%li %i is eating\n", check_time(), philo->num);
+	pthread_mutex_unlock(&philo->table->msg_lock);
 	philo->ate++;
 	usleep(philo->table->t_eat * 1000);
 }
@@ -42,9 +52,22 @@ void	eat_spaghetti(t_philo *philo)
 void	put_down_forks(t_philo *philo)
 {
 	pthread_mutex_unlock(philo->fork_left);
+	pthread_mutex_lock(&philo->table->msg_lock);
 	printf("%li %i put a fork down (left)\n", check_time(), philo->num);
+	pthread_mutex_unlock(&philo->table->msg_lock);
 	pthread_mutex_unlock(philo->fork_right);
+	pthread_mutex_lock(&philo->table->msg_lock);
 	printf("%li %i put a fork down (right)\n", check_time(), philo->num);
+	pthread_mutex_unlock(&philo->table->msg_lock);
+}
+
+//sleep
+void	take_a_nap(t_philo *philo)
+{
+	pthread_mutex_lock(&philo->table->msg_lock);
+	printf("%li %i is sleeping\n", check_time(), philo->num);
+	pthread_mutex_unlock(&philo->table->msg_lock);
+	usleep(philo->table->t_sleep * 1000);
 }
 
 void	*start_routine(void *arg)
@@ -56,10 +79,10 @@ void	*start_routine(void *arg)
 	pick_up_forks(philo);
 	eat_spaghetti(philo);
 	put_down_forks(philo);
-	//sleep
-	printf("%li %i is sleeping\n", check_time(), philo->num);
-	usleep(philo->table->t_sleep * 1000);
+	take_a_nap(philo);
 	//think
+	pthread_mutex_lock(&philo->table->msg_lock);
 	printf("%li %i is thinking\n", check_time(), philo->num);
+	pthread_mutex_unlock(&philo->table->msg_lock);
 	return (arg);
 }
