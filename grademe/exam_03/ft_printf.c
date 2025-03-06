ef0546192be12ba2917cd5faae8c601b2b8546ee
frnki42 <femboeckelia@googlemail.com>
2025-03-06 22:52:09 +0100

    added printf_main PR: 2:13

---
+++ grademe/exam_03/ft_printf.c
@@ -0,0 +1,28 @@
+int	ft_printf(const char *format, ...)
+{
+	va_list	args;
+	int	amount;
+	int	tmp;
+
+	if (!format)
+		return (-1);
+	va_start(args, format);
+	amount = 0;
+	while (*format)
+	{
+		if (*format == '%')
+		{
+			tmp = handle_format(++format, args);
+			if (tmp == -1)
+				return (-1);
+			amonut += tmp;
+		}
+		else if (write(1, format, 1) == -1)
+			return (-1);
+		else
+			amount++;
+		format++;
+	}
+	va_end(args);
+	return (amount);
+}
